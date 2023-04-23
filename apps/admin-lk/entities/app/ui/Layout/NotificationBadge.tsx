7493d941cfae628da116ee9705f9ed2c48eb27eb
Devisione <stipafk@gmail.com>
2023-04-23 22:24:24 +0200

    change global components

--- apps/admin-lk/entities/app/ui/Layout/NotificationBadge.tsx
+++ apps/admin-lk/entities/app/ui/Layout/NotificationBadge.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connectContext } from "context-base-api";
-import { AppStore } from "../../entities/app/model/store";
+import { AppStore } from "../../model/store";
 import { Badge, Button } from "@project/ui-kit";
 import { SoundOutlined } from "@project/ui-kit/icons";
 
--- apps/admin-lk/pages/_app.tsx
+++ apps/admin-lk/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "./components/ui/index.css";
 import type { AppProps } from "next/app";
 import { NextPageWithLayout } from "../entities/next/model/types";
-import LayoutWrapper from "../ui/Layout/Layout";
+import LayoutWrapper from "../entities/app/ui/Layout/Layout";
 import { AppStore } from "../entities/app/model/store";
 
 type AppPropsWithLayout = AppProps & {
